48c2f991984ec9a1ebf0fe9dbdbcd256fb08196d
MassiveManM8 <88780376+MassiveManM8@users.noreply.github.com>
2022-03-10 15:14:46 +0100

    Add files via upload

---
+++ random random random/script.js
@@ -0,0 +1,91 @@
+const randBtn = document.getElementById("rand-btn")
+
+randBtn.addEventListener("click", ()=> {
+    const randnum = Math.floor(Math.random(11) * 11) + 1
+    console.log(randnum)
+
+
+    switch (randnum) {
+        case 1 :
+            window.open("https://coolors.co/82ff9e-a9fbc3-b594b6-935fa7-9b489b")
+            console.log("webiste 1")
+            break;
+        case 2 :
+            window.open("https://www.mathgoodies.com/calculators/random_no_custom")
+            console.log("webiste 2")
+            break;
+        case 3 :
+            window.open("https://randomwordgenerator.com/")
+            console.log("webiste 3")
+            break;
+        case 4 :
+            window.open("https://servc.eu/rsg/")
+            console.log("webiste 4")
+            break;
+        case 5 :
+            window.open("https://gamegauntlets.com/")
+            console.log("webiste 5")
+            break;
+        case 6 :
+            window.open("https://www.behindthename.com/random/")
+            console.log("webiste 6")
+            break;
+        case 7 :
+            window.open("https://www.generatormix.com/random-flag")
+            console.log("webiste 7")
+            break;
+        case 8 :
+            window.open("https://passwordsgenerator.net/")
+            console.log("webiste 8")
+            break;   
+        case 9 :
+            window.open("https://www.random.org/strings/?num=10&len=10&digits=on&unique=on&format=html&rnd=new")
+            console.log("webiste 9")
+            break;            
+        case 10 :
+            window.open("https://www.rangen.co.uk/chars/quickchargen.php")
+            console.log("webiste 10")
+            break;       
+        case 11 :
+            window.open("index.html")
+            console.log("webiste 11")
+            break;     
+        case 12 :
+            window.open("https://faculty.washington.edu/ejslager/random-generator/index.html")
+            console.log("webiste 12")
+            break;                               
+        case 13 :
+            window.open("https://www.mentalfloss.com/amazingfactgenerator/554086")
+            console.log("webiste 13")
+            break;                               
+        case 14 :
+            window.open("https://generated.photos/face-generator/new#")
+            console.log("webiste 14")
+            break;                                                             
+        case 15 :
+            window.open("https://randomwordgenerator.com/picture.php")
+            console.log("webiste 15")
+            break;                               
+        case 16 :
+            window.open("https://picsum.photos/")
+            console.log("webiste 16")
+            break;                               
+        case 17 :
+            window.open("https://perchance.org/object")
+            console.log("webiste 17")
+            break;                               
+        case 18 :
+            window.open("https://www.youtuberandom.com/?watch=II7y45YNWyI")
+            console.log("webiste 18")
+            break;                               
+        case 19 :
+            window.open("https://www.zufallspokemon.de/en/")
+            console.log("webiste 19")
+            break;                               
+        case 20 :
+            window.open("https://www.randomlists.com/random-languages")
+            console.log("webiste 20")
+            break;                               
+
+    }
+})
